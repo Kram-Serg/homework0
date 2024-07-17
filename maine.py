816061f089f9823f2fbbabffc1a56982ae2c7120
Kram-Serg <bild003@yandex.ru>
2024-07-17 23:00:29 +0700

    Add files via upload

---
+++ maine.py
@@ -0,0 +1,20 @@
+# 1st program
+print(9**0.5*5)
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+# 3rd program
+n =1234
+n1 = 1234%1000//10
+m = 5678
+m1 = 5678%1000//10
+print(n1 + m1)
+# 4th program
+n = 13.42
+n1 = int(n)
+n2 = n%1*100
+n3 = (round(n2,2))
+m = 42.13
+m1 = int(m)
+m2 = m%1*100
+m3 = (round(m2,2))
+print(n1==m3 or m1==n3)
